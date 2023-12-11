e9adf2c69c6a4cf780f88dcabe3a692a88eae23f
Henry Wilder <henrythepony@gmail.com>
2023-12-11 02:34:55 -0500

    GOT IT WORKING (sorta)

--- src/scripts/popup.ts
+++ src/scripts/popup.ts
@@ -1,8 +1,18 @@
+const isModActiveInput = document.getElementById('is-mod-active-button') as HTMLButtonElement | null;
+const saveButton = document.getElementById('save-button') as HTMLButtonElement | null;
+
+if (isModActiveInput === null || saveButton === null) throw new Error('Critical element missing');
+
+isModActiveInput.addEventListener('click', () => {
+    isModActiveInput.classList.toggle('secondary', !isModActiveInput.classList.toggle('primary'));
+    setModActive(isModActiveInput.classList.contains('primary'));
+});
+
 let isModActive: any;
 
 export const loadSettings = async () => {
     isModActive = await browser.storage.sync.get('is-mod-active');
-    toggleButtonState(isModActiveInput, isModActive);
+    isModActiveInput.classList.toggle('secondary', !isModActiveInput.classList.toggle('primary', isModActive));
 };
 
 export const saveSettings = () => {
@@ -10,3 +20,6 @@ export const saveSettings = () => {
 };
 
 export const setModActive = (value: any) => (isModActive = value);
+
+loadSettings();
+saveButton.addEventListener('click', saveSettings);
